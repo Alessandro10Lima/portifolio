35029c4d28e62e4ec6990f1eda403ffdd000c025
alessanrolima0292@gmail.com <alessandrolima0292@gmail.com>
2023-07-15 10:54:55 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Alessandro10Lima.png" />
-    <Titulo fontSize={20}>Alessandro Lima</Titulo>
+    <SideContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Alessandro</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Alessandro10Lima
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideContainer>
   </aside>
 )
 
